9dc96ea1129aacfa73e172c387a470d6a63b5dc9
datadirr <datadirr@gmail.com>
2024-09-21 00:07:33 +0530

    sign in

--- lib/auth/sign_in_screen.dart
+++ lib/auth/sign_in_screen.dart
@@ -20,44 +20,55 @@ class _SignInScreenState extends State<SignInScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: SafeArea(
-          child: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Column(mainAxisSize: MainAxisSize.min, children: [
-              const VSpace(space: 20),
-              Image.asset(Assets.imgDatadirrTxt,
-                  width: 100, package: Plugin.package),
-              const VSpace(space: 20),
-              Text(Strings.signIn,
-                  style: Styles.txtRegular(fontSize: Fonts.fontXXLarge)),
-              const VSpace(),
-              Text(Strings.signInMsg, style: Styles.txtRegular()),
-              const VSpace(space: 30),
-              CATextField(
-                controller: _conEmail,
-                hintText: Strings.email,
-              ),
-              const VSpace(space: 30),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+            seedColor: Colorr.primary,
+            primary: Colorr.primary,
+            surface: Colorr.white),
+        scaffoldBackgroundColor: Colorr.white,
+        useMaterial3: true,
+      ),
+      home: Scaffold(
+        body: SafeArea(
+            child: Padding(
+          padding: const EdgeInsets.all(20),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              Column(mainAxisSize: MainAxisSize.min, children: [
+                const VSpace(space: 20),
+                Image.asset(Assets.imgDatadirrTxt,
+                    width: 100, package: Plugin.package),
+                const VSpace(space: 20),
+                Text(Strings.signIn,
+                    style: Styles.txtRegular(fontSize: Fonts.fontXXLarge)),
+                const VSpace(),
+                Text(Strings.signInMsg, style: Styles.txtRegular()),
+                const VSpace(space: 30),
+                CATextField(
+                  controller: _conEmail,
+                  hintText: Strings.email,
+                ),
+                const VSpace(space: 30),
+                Align(
+                    alignment: Alignment.centerLeft,
+                    child: Tap(
+                        onTap: () {},
+                        child: Text(Strings.createAccount,
+                            style: Styles.txtRegular(color: Colorr.primaryBlue))))
+              ]),
               Align(
-                  alignment: Alignment.centerLeft,
-                  child: Tap(
-                      onTap: () {},
-                      child: Text(Strings.createAccount,
-                          style: Styles.txtRegular(color: Colorr.primaryBlue))))
-            ]),
-            Align(
-              alignment: Alignment.centerRight,
-              child: FlexWidth(
-                child: CButton(text: Strings.next, onTap: () {}),
-              ),
-            )
-          ],
-        ),
-      )),
+                alignment: Alignment.centerRight,
+                child: FlexWidth(
+                  child: CButton(text: Strings.next, onTap: () {}),
+                ),
+              )
+            ],
+          ),
+        )),
+      ),
     );
   }
 }
